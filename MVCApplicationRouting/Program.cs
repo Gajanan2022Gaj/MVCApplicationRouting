e0c593f7e4589313da8a37bb4328e34623fd6783
Gajanan2022Gaj <112186028+Gajanan2022Gaj@users.noreply.github.com>
2023-01-18 17:14:07 +0530

    Update Program.cs

--- MVCApplicationRouting/Program.cs
+++ MVCApplicationRouting/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace MVCApplicationRouting
-{
+{//comment added
     public class Program
     {
         public static void Main(string[] args)
