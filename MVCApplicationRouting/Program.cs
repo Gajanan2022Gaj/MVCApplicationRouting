70912c967eab2f1c47cc591b2d205f27f0620df1
Jayu202121 <Jayu202121@outlook.com>
2023-01-18 20:23:47 +0530

    te

--- MVCApplicationRouting/Program.cs
+++ MVCApplicationRouting/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 namespace MVCApplicationRouting
 {
     //comment added
-    public class Program2
+    public class Program
     {
         public static void Main(string[] args)
         {
