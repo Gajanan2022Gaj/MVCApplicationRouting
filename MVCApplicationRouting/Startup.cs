bb39af24b05a7c75717e0104e93046c7a75522c0
Jayu202121 <Jayu202121@outlook.com>
2023-01-18 22:26:33 +0530

    secondbr

--- MVCApplicationRouting/Startup.cs
+++ MVCApplicationRouting/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+// Second br change
 namespace MVCApplicationRouting
 {
     public class Startup
